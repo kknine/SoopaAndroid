dba8de628646b31dba5c3fc896f825d091d2ee54
Krzysztof Kacprzyk <kr.kacprzyk@gmail.com>
2018-11-17 23:35:50 +0000

    Add navigation along straight line and along streets

--- app/src/main/java/example/com/soopa/MapsActivity.java
+++ app/src/main/java/example/com/soopa/MapsActivity.java
@@ -1,7 +1,9 @@
 package example.com.soopa;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
@@ -12,6 +14,9 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,12 +29,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.maps.model.TileOverlay;
 import com.google.android.gms.maps.model.TileOverlayOptions;
 import com.google.maps.android.heatmaps.HeatmapTileProvider;
 
 import org.json.JSONException;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -45,58 +53,65 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private Server mServer;
     private TileOverlay mOverlay;
     private Marker currentMarker;
-    private ArrayList<Marker> previousCrimeMarkers = new ArrayList<>();
-    private ArrayList<Marker> previousVantageMarkers = new ArrayList<>();
-    private HashMap<String, VantagePoint> previousVantagePoints = new HashMap<>();
-    private HashMap<String, Crime> previousCrimes = new HashMap<>();
+    private ArrayList<Marker> displayedCrimeMarkers = new ArrayList<>();
+    private ArrayList<Marker> displayedVantageMarkers = new ArrayList<>();
+    private HashMap<String, VantagePoint> displayedVantagePoints = new HashMap<>();
+    private HashMap<String, Crime> displayedCrimes = new HashMap<>();
     private boolean done = false;
     private boolean locationSet = false;
     private LocationManager mLocationManager;
     private LocationListener mLocationListener;
     private Marker currentLocation;
     public static final int LOCATION_REFRESH_PERIOD = 2000;
+    public static final String[] SUPERHEROES = {"Iron Man", "Spider-Man", "Hulk"};
+    private Polyline route;
+    private int currentSuperNum = 0;
+    private String chosenVantagePointId = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                updateSuperHero();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         mServer = new Server(this);
-        // Acquire a reference to the system Location Manager
-        mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
-        // Define a listener that responds to location updates
+        mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         mLocationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
                 // Called when a new location is found by the network location provider.
                 updateCurrentLocation(new LatLng(location.getLatitude(), location.getLongitude()));
                 if (!locationSet) {
                     if (done) {
-                        LatLngBounds.Builder builder = new LatLngBounds.Builder();
-                        for (Marker marker2 : previousCrimeMarkers) {
-                            builder.include(marker2.getPosition());
+                        ArrayList<LatLng> points = new ArrayList<>();
+                        for(Marker crime : displayedCrimeMarkers) {
+                            points.add(crime.getPosition());
                         }
-                        builder.include(new LatLng(location.getLatitude(), location.getLongitude()));
-                        LatLngBounds bounds = builder.build();
-                        int width = getResources().getDisplayMetrics().widthPixels;
-                        int height = getResources().getDisplayMetrics().heightPixels;
-                        int padding = (int) (width * 0.2); // offset from edges of the map 20% of screen
-                        mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding));
+                        points.add(currentLocation.getPosition());
+                        centerCamera(points);
                         locationSet = true;
-                        System.out.println("OKOKOKO");
                     }
                 }
             }
-
             public void onStatusChanged(String provider, int status, Bundle extras) {
             }
-
             public void onProviderEnabled(String provider) {
             }
-
             public void onProviderDisabled(String provider) {
             }
         };
@@ -151,26 +166,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
-            public boolean onMarkerClick(Marker marker) {
-                if(previousCrimes.containsKey(marker.getId())) {
+            public boolean onMarkerClick(final Marker marker) {
+                if(displayedCrimes.containsKey(marker.getId())) {
                     cleanPreviousVantagePoints();
-                    addVantagePoints(previousCrimes.get(marker.getId()).getVantagePoints());
+                    addVantagePoints(displayedCrimes.get(marker.getId()).getVantagePoints());
                     LatLngBounds.Builder builder = new LatLngBounds.Builder();
-                    for (Marker vP : previousVantageMarkers) {
+                    for (Marker vP : displayedVantageMarkers) {
                         builder.include(vP.getPosition());
                     }
                     builder.include(marker.getPosition());
@@ -179,6 +185,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     int height = getResources().getDisplayMetrics().heightPixels;
                     int padding = (int) (width * 0.2); // offset from edges of the map 20% of screen
                     mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding));
+                } else if(displayedVantageMarkers.contains(marker)) {
+                    final VantagePoint vP = displayedVantagePoints.get(marker.getId());
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MapsActivity.this);
+                    builder.setTitle("Tall building");
+                    builder.setMessage("Height: " + Double.toString(vP.getHeight()) + " meters");
+                    builder.setPositiveButton("Navigate", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            navigate(marker.getId());
+                        }
+                    });
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+
+
                 }
                 marker.showInfoWindow();
                 return true;
@@ -201,19 +228,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void cleanPreviousCrimes() {
-        for(Marker marker : previousCrimeMarkers) {
+        for(Marker marker : displayedCrimeMarkers) {
             marker.remove();
         }
-        previousCrimeMarkers.clear();
-        previousCrimes.clear();
+        displayedCrimeMarkers.clear();
+        displayedCrimes.clear();
     }
 
     private void cleanPreviousVantagePoints() {
-        for(Marker vantagePoint : previousVantageMarkers) {
+        for(Marker vantagePoint : displayedVantageMarkers) {
             vantagePoint.remove();
         }
-        previousVantageMarkers.clear();
-        previousVantagePoints.clear();
+        displayedVantageMarkers.clear();
+        displayedVantagePoints.clear();
     }
 
     private void addCrimes(ArrayList<Crime> crimeList) {
@@ -228,12 +255,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             markerOptions.title(crime.getCrimeType());
             markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallIcon));
             Marker marker = mMap.addMarker(markerOptions);
-            previousCrimeMarkers.add(marker);
-            previousCrimes.put(marker.getId(),crime);
+            displayedCrimeMarkers.add(marker);
+            displayedCrimes.put(marker.getId(),crime);
         }
         if(!done) {
             LatLngBounds.Builder builder = new LatLngBounds.Builder();
-            for (Marker marker2 : previousCrimeMarkers) {
+            for (Marker marker2 : displayedCrimeMarkers) {
                 builder.include(marker2.getPosition());
             }
             LatLngBounds bounds = builder.build();
@@ -248,31 +275,180 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void addVantagePoints(ArrayList<VantagePoint> vantagePoints) {
+        int iconHeight = 100;
+        int iconWidth = 100;
+        BitmapDrawable originalIcon = (BitmapDrawable)getResources().getDrawable(R.drawable.building);
+        Bitmap b = originalIcon.getBitmap();
+        Bitmap smallIcon = Bitmap.createScaledBitmap(b, iconWidth, iconHeight, false);
         for(VantagePoint vantagePoint : vantagePoints) {
             MarkerOptions markerOptions = new MarkerOptions();
             markerOptions.position(vantagePoint.getPosition());
             markerOptions.title(Double.toString(vantagePoint.getHeight()));
+            markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallIcon));
             Marker marker = mMap.addMarker(markerOptions);
-            previousVantageMarkers.add(marker);
-            previousVantagePoints.put(marker.getId(),vantagePoint);
+            displayedVantageMarkers.add(marker);
+            displayedVantagePoints.put(marker.getId(),vantagePoint);
         }
     }
 
     private void updateCurrentLocation(LatLng position) {
-        int iconHeight = 100;
-        int iconWidth = 100;
-        BitmapDrawable originalIcon = (BitmapDrawable)getResources().getDrawable(R.drawable.current_location);
-        Bitmap b = originalIcon.getBitmap();
-        Bitmap smallIcon = Bitmap.createScaledBitmap(b, iconWidth, iconHeight, false);
         if(currentLocation != null) {
             Marker marker = currentLocation;
             MarkerAnimation.animateMarkerToICS(marker, position, new LatLngInterpolator.Spherical());
         } else {
             MarkerOptions markerOptions = new MarkerOptions();
             markerOptions.position(position);
+            int iconHeight = 128;
+            int iconWidth = 128;
+            int superNum = getSuperHeroNum();
+            int resource = R.drawable.current_location;
+            switch(superNum) {
+                case 0:
+                    resource = R.drawable.iron_man;
+                    break;
+                case 1:
+                    resource = R.drawable.spider_man;
+                    break;
+                case 2:
+                    resource = R.drawable.hulk;
+                    break;
+            }
+            BitmapDrawable originalIcon = (BitmapDrawable)getResources().getDrawable(resource);
+            Bitmap b = originalIcon.getBitmap();
+            Bitmap smallIcon = Bitmap.createScaledBitmap(b, iconWidth, iconHeight, false);
             markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallIcon));
             currentLocation = mMap.addMarker(markerOptions);
         }
+    }
+
+    public void showMeAndCrimes(View v) {
+        ArrayList<LatLng> points = new ArrayList<>();
+        for(Marker crime : displayedCrimeMarkers) {
+            points.add(crime.getPosition());
+        }
+        points.add(currentLocation.getPosition());
+        centerCamera(points);
+    }
+
+    private void navigate(String vPId) {
+        int superNum = getSuperHeroNum();
+        chosenVantagePointId = vPId;
+        final LatLng target = displayedVantagePoints.get(vPId).getPosition();
+        switch (superNum) {
+            case 0: // Iron Man
+                if (route != null)
+                    route.remove();
+                ArrayList<LatLng> points = new ArrayList<>();
+                points.add(currentLocation.getPosition());
+                points.add(target);
+                centerCamera(points);
+                route = addPolyline(points);
+                break;
+            case 1: // Spider-Man
+//                mServer.getDirections(new ServerUtils.Callback<ArrayList<LatLng>, String>() {
+//                    @Override
+//                    public void onSuccess(ArrayList<LatLng> points) {
+//                        if (route != null)
+//                            route.remove();
+//                        route = addPolyline(points);
+//                        centerCamera(points);
+//                    }
+//
+//                    @Override
+//                    public void onFail(String obj) {
+//
+//                    }
+//                }, currentLocation.getPosition(), target);
+                mServer.getSessionId(new ServerUtils.Callback<String, String>() {
+                    @Override
+                    public void onSuccess(final String sessionId) {
+                        mServer.getRouteShape(new ServerUtils.Callback<ArrayList<LatLng>, String>() {
+                            @Override
+                            public void onSuccess(ArrayList<LatLng> points) {
+                                if (route != null)
+                                    route.remove();
+                                route = addPolyline(points);
+                                centerCamera(points);
+                            }
+
+                            @Override
+                            public void onFail(String obj) {
+
+                            }
+                        },target,sessionId);
+                    }
+
+                    @Override
+                    public void onFail(String obj) {
+
+                    }
+                },currentLocation.getPosition(), target);
+                break;
+            case 2: // Hulk
+                break;
+        }
+
+
+    }
+
+    private Polyline addLineSegment(LatLng point1, LatLng point2) {
+        PolylineOptions polylineOptions = new PolylineOptions();
+        polylineOptions.add(point1).add(point2);
+        return mMap.addPolyline(polylineOptions);
+    }
+
+    private Polyline addPolyline(ArrayList<LatLng> points) {
+        PolylineOptions polylineOptions = new PolylineOptions();
+        for(LatLng point : points) {
+            polylineOptions.add(point);
+        }
+        return mMap.addPolyline(polylineOptions);
+    }
+
+
+    private void centerCamera(ArrayList<LatLng> points) {
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for (LatLng point : points) {
+            builder.include(point);
+        }
+        LatLngBounds bounds = builder.build();
+        int width = getResources().getDisplayMetrics().widthPixels;
+        int height = getResources().getDisplayMetrics().heightPixels;
+        int padding = (int) (width * 0.2); // offset from edges of the map 20% of screen
+        mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding),1000,null);
+    }
+
+    private int getSuperHeroNum() {
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        int pos = spinner.getSelectedItemPosition();
+        return pos;
+    }
+
+    private void updateSuperHero() {
+        int iconHeight = 128;
+        int iconWidth = 128;
+        int superNum = getSuperHeroNum();
+        int resource = R.drawable.current_location;
+        switch(superNum) {
+            case 0:
+                resource = R.drawable.iron_man;
+                break;
+            case 1:
+                resource = R.drawable.spider_man;
+                break;
+            case 2:
+                resource = R.drawable.hulk;
+                break;
+        }
+        BitmapDrawable originalIcon = (BitmapDrawable)getResources().getDrawable(resource);
+        Bitmap b = originalIcon.getBitmap();
+        Bitmap smallIcon = Bitmap.createScaledBitmap(b, iconWidth, iconHeight, false);
+        currentLocation.setIcon(BitmapDescriptorFactory.fromBitmap(smallIcon));
+        if((superNum != currentSuperNum)&&(chosenVantagePointId!=null)) {
+            navigate(chosenVantagePointId);
+            currentSuperNum = superNum;
+        }
+
     }
 
     private void addHeatMap() {
--- app/src/main/java/example/com/soopa/server/Server.java
+++ app/src/main/java/example/com/soopa/server/Server.java
@@ -2,12 +2,17 @@ package example.com.soopa.server;
 
 import android.content.Context;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.model.LatLng;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -79,7 +84,109 @@ public class Server {
 //        }
         LatLng building = new LatLng(51.6,0);
         callback.onSuccess(building);
+    }
 
+    public void getDirections(final ServerUtils.Callback<ArrayList<LatLng>,String> callback, LatLng from, LatLng to) {
+        if(ServerUtils.isReadyForQuery(context)) {
+            String fromString = "from="+Double.toString(from.latitude)+","+Double.toString(from.longitude);
+            String toString = "to="+Double.toString(to.latitude)+","+Double.toString(to.longitude);
+            String url = ServerConstants.DIRECTIONS_API_URL + "&" + fromString + "&" + toString;
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject jsonObject) {
+                            ArrayList<LatLng> points = new ArrayList<>();
+                            try {
+                                JSONObject routeJSON = jsonObject.getJSONObject("route");
+                                JSONArray legsJSONArray = routeJSON.getJSONArray("legs");
+                                JSONObject legJSON = legsJSONArray.getJSONObject(0);
+                                JSONArray maneuversJSONArray = legJSON.getJSONArray("maneuvers");
+                                for (int i = 0; i < maneuversJSONArray.length(); i++) {
+                                    JSONObject maneuver = maneuversJSONArray.getJSONObject(i);
+                                    JSONObject startPointJSON = maneuver.getJSONObject("startPoint");
+                                    LatLng point = new LatLng(startPointJSON.getDouble("lat"), startPointJSON.getDouble("lng"));
+                                    points.add(point);
+                                }
+                            } catch(Exception e) {
+                                e.printStackTrace();
+                            }
+                            callback.onSuccess(points);
+                        }
+                    }, new ServerUtils.ServerResponse<ArrayList<LatLng>>().simpleError(callback)) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+            };
+            mRequestQueue.add(jsonObjectRequest);
+        } else {
+            callback.onFail(ServerConstants.CONNECTION_ERROR);
+        }
+    }
+    public void getSessionId(final ServerUtils.Callback<String,String> callback, LatLng from, LatLng to) {
+        if(ServerUtils.isReadyForQuery(context)) {
+            String fromString = "from="+Double.toString(from.latitude)+","+Double.toString(from.longitude);
+            String toString = "to="+Double.toString(to.latitude)+","+Double.toString(to.longitude);
+            String url = ServerConstants.DIRECTIONS_API_URL + "&" + fromString + "&" + toString;
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject jsonObject) {
+                            ArrayList<LatLng> points = new ArrayList<>();
+                            String session = "";
+                            try {
+                                JSONObject routeJSON = jsonObject.getJSONObject("route");
+                                session = routeJSON.getString("sessionId");
+
+                            } catch(Exception e) {
+                                e.printStackTrace();
+                            }
+                            callback.onSuccess(session);
+                        }
+                    }, new ServerUtils.ServerResponse<String>().simpleError(callback)) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+            };
+            mRequestQueue.add(jsonObjectRequest);
+        } else {
+            callback.onFail(ServerConstants.CONNECTION_ERROR);
+        }
+    }
+    public void getRouteShape(final ServerUtils.Callback<ArrayList<LatLng>,String> callback, LatLng from, String sessionId) {
+        if(ServerUtils.isReadyForQuery(context)) {
+            String url = ServerConstants.ROUTE_SHAPE_API_URL + "&sessionId="+sessionId+"&mapWidth=320&mapHeight=240&mapScale=1733371&mapLat="+Double.toString(from.latitude)+"&mapLng="+Double.toString(from.longitude);
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject jsonObject) {
+                            ArrayList<LatLng> points = new ArrayList<>();
+                            try {
+                                JSONObject routeJSON = jsonObject.getJSONObject("route");
+                                JSONObject shapeJSON = routeJSON.getJSONObject("shape");
+                                JSONArray shapePointsJSONArray = shapeJSON.getJSONArray("shapePoints");
+                                for (int i = 0; i < shapePointsJSONArray.length(); i=i+2) {
+                                    double lat = shapePointsJSONArray.getDouble(i);
+                                    double lng = shapePointsJSONArray.getDouble(i+1);
+                                    LatLng point = new LatLng(lat,lng);
+                                    points.add(point);
+                                }
+                            } catch(Exception e) {
+                                e.printStackTrace();
+                            }
+                            callback.onSuccess(points);
+                        }
+                    }, new ServerUtils.ServerResponse<ArrayList<LatLng>>().simpleError(callback)) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+            };
+            mRequestQueue.add(jsonObjectRequest);
+        } else {
+            callback.onFail(ServerConstants.CONNECTION_ERROR);
+        }
     }
 
 
